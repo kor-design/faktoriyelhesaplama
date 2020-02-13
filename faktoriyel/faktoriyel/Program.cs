89f166459bedfa11330ecad5d0a6f84857885d76
omer kor <omerkorr@gmail.com>
2020-02-13 09:18:16 +0300

    last

--- faktoriyel/faktoriyel/Program.cs
+++ faktoriyel/faktoriyel/Program.cs
@@ -10,6 +10,23 @@ namespace faktoriyel
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Faktoriyeli alınacak sayıyı giriniz");
+                var fakt_sayi = int.Parse(Console.ReadLine());
+                var baslangic = 1;
+                var ilk_deger = fakt_sayi;
+                while (fakt_sayi > 0)
+                {
+                    if (fakt_sayi > 0)
+                    {
+                        baslangic *= fakt_sayi;
+                        fakt_sayi--;
+                    }
+                }
+                Console.WriteLine("{0} sayısının faktoriyeli = {1}", ilk_deger, baslangic);
+            }
         }
+
     }
 }
